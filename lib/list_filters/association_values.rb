fc033a5adf51d14985713a470882658bcb017f20
Woody Peterson <woody.peterson@gmail.com>
2009-01-11 00:20:32 -0800

    added :through => :association option to association_values filter, which basically allows filtering by values in an association of an association (i.e. doing a join)

--- lib/list_filters/association_values.rb
+++ lib/list_filters/association_values.rb
@@ -1,5 +1,12 @@
 class AssociationValues < ActiveScaffold::DataStructures::ListFilter
   def conditions(params)
-    return ["#{options[:table_name]}.#{options[:field]} IN (?)", params]
+    add_through_option_to_includes if options[:through]
+    return ["`#{options[:table_name]}`.`#{options[:field]}` IN (?)", params]
+  end
+  
+  def add_through_option_to_includes
+    if !@core.columns[options[:through]].includes.include?(options[:table_name])
+      @core.columns[options[:through]].includes << options[:table_name] 
+    end
   end
 end
--- lib/list_filters/values.rb
+++ lib/list_filters/values.rb
@@ -2,7 +2,7 @@ class Values < ActiveScaffold::DataStructures::ListFilter
 
 	# Return a list of conditions based on the params 
 	def conditions(params)
-		return ["#{@core.model.table_name}.#{options[:field]} IN (?)", params]
+		return ["`#{@core.model.table_name}`.`#{options[:field]}` IN (?)", params]
 	end
 	
 end
