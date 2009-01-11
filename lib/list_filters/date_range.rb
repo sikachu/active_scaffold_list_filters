b35ee5f269d4085e7dda2577758566a0b1837dac
Woody Peterson <woody.peterson@gmail.com>
2009-01-11 02:05:22 -0800

    fixed a bug in my last bugfix

--- lib/list_filters/date_range.rb
+++ lib/list_filters/date_range.rb
@@ -4,13 +4,13 @@ class DateRange < ActiveScaffold::DataStructures::ListFilter
   # Return a list of conditions based on the params 
   def conditions(params)
     if params[:text_start]
-      date_start = params[:text_start].to_date + " 00:00:00"
+      date_start = params[:text_start].to_date.to_s + " 00:00:00"
     else
       date_start = "#{params["start"]["year"]}-#{params["start"]["month"]}-#{params["start"]["day"]} 00:00:00"
     end
     
     if params[:text_end]
-      date_end = params[:text_end].to_date + " 23:59:59"
+      date_end = params[:text_end].to_date.to_s + " 23:59:59"
     else
       date_end = "#{params["end"]["year"]}-#{params["end"]["month"]}-#{params["end"]["day"]} 23:59:59"
     end
