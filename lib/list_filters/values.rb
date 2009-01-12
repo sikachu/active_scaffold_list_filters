bb7925dae05937d623143137c9e2c7304e57f3af
Woody Peterson <woody.peterson@gmail.com>
2009-01-11 20:06:02 -0800

    made values automatically handle true and false as booleans. This could be a problem if someone wanted to look for the stings true and false, but that is an edge case whose support can be developed when needed

--- lib/list_filters/values.rb
+++ lib/list_filters/values.rb
@@ -1,7 +1,9 @@
+require 'ruby-debug'
 class Values < ActiveScaffold::DataStructures::ListFilter
   def conditions(params)
     add_through_option_to_includes if options[:through]
     table_name = options[:table_name] || @core.model.table_name
+    booleanize_values(params)
     return ["`#{table_name}`.`#{options[:field]}` IN (?)", params]
   end
   
@@ -9,5 +11,10 @@ class Values < ActiveScaffold::DataStructures::ListFilter
     if !@core.columns[options[:through]].includes.include?(options[:table_name])
       @core.columns[options[:through]].includes << options[:table_name] 
     end
+  end
+  
+  def booleanize_values(array)
+    array << true if array.delete("true")
+    array << false if array.delete("false")
   end
 end
